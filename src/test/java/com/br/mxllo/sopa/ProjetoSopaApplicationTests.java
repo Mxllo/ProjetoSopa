e31a0de3efd33b38b2a0209f5c54402e64492955
Mxllo <lucasmellog@gmail.com>
2022-06-27 21:21:45 -0300

    Adicionando projeto spring

---
+++ src/test/java/com/br/mxllo/sopa/ProjetoSopaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.mxllo.sopa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSopaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
